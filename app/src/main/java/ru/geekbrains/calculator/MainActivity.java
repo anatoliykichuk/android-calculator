c7fbe41d52eafcd047d4ec5ba1e922d4121c6cdf
anatoliy.kichuk <anatoliy.kichuk@gmail.com>
2021-06-05 16:00:29 +0300

    Задание 3: Частично реализована обработка нажатий на кнопки.

--- app/src/main/java/ru/geekbrains/calculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/calculator/MainActivity.java
@@ -3,12 +3,48 @@ package ru.geekbrains.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setOnClickListeners();
     }
+
+    private void setOnClickListeners() {
+        ArrayList<View> buttons = ((FrameLayout) findViewById(R.id.keyboard)).getTouchables();
+
+        for (View button:buttons) {
+            button.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        CharSequence buttonText = ((MaterialButton) v).getText();
+
+        TextView scoreBoard = findViewById(R.id.scoreboard);
+        scoreBoard.append(buttonText);
+
+
+    }
+
+    private boolean isNumber(CharSequence buttonText) {
+
+        return false;
+    }
+
+
+
 }
